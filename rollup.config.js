8714b296e59206727252925a4121b17c2d22553f
Rajan Singh <rajan1992@hotmail.com>
2020-11-09 16:58:42 -0500

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,57 @@
+import commonjs from 'rollup-plugin-commonjs';
+import livereload from 'rollup-plugin-livereload';
+import resolve from 'rollup-plugin-node-resolve';
+import svelte from 'rollup-plugin-svelte';
+import sveltePreprocess from 'svelte-preprocess';
+const { sveltePreprocessConfig } = require('./svelte-preprocess.config');
+
+function serve() {
+	let server;
+
+	function toExit() {
+		if (server) server.kill(0);
+	}
+
+	return {
+		writeBundle() {
+			if (server) return;
+			server = require('child_process').spawn(
+				'npm',
+				['run', 'start', '--', '--dev'],
+				{
+					stdio: ['ignore', 'inherit', 'inherit'],
+					shell: true,
+				}
+			);
+
+			process.on('SIGTERM', toExit);
+			process.on('exit', toExit);
+		},
+	};
+}
+
+export default {
+	input: 'src/main.js',
+	output: {
+		sourcemap: true,
+		format: 'iife',
+		name: 'app',
+		file: 'public/bundle.js',
+	},
+	plugins: [
+		svelte({
+			dev: true,
+			preprocess: sveltePreprocess(sveltePreprocessConfig),
+		}),
+		resolve({
+			browser: true,
+			dedupe: ['svelte'],
+		}),
+		commonjs(),
+		serve(),
+		livereload('public'),
+	],
+	watch: {
+		clearScreen: false,
+	},
+};
